236ac177c633d5f2cd24c470024714e4a180d896
Rudraksh Sharma <sharmar19@outlook.com>
2026-02-09 17:49:20 +0530

    Day 8

---
+++ day8q2.c
@@ -0,0 +1,10 @@
+//https://leetcode.com/problems/power-of-two/
+
+bool isPowerOfTwo(int n) {
+    if (n<=0) return 0; 
+    if (n==1) return 1; 
+
+    if (n%2!=0) return 0;
+    
+    return isPowerOfTwo(n/2);
+}
